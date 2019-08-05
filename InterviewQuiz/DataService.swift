f2610789e0ececbecc488d1feb5f761bf3f8b2b9
Nikita Koniukh <koniukh.nikita@gmail.com>
2019-08-05 20:49:07 +0300

    button logic

--- InterviewQuiz/DataService.swift
+++ InterviewQuiz/DataService.swift
@@ -9,17 +9,31 @@
 import Foundation
 class DataService{
 
-    static var instance = DataService()
+    private(set) var questions = [QuestionModel]()
 
-    func getQuestion() -> [QuestionModel] {
-        var questions = [QuestionModel]()
-        questions.append(QuestionModel(
-            questionTitle: "What output will be produced by the code below?",
-            questionImage: "first.jpg",
-            answers: ["This is a subset","This is not a subset","Nothing will be output","This code will compile but crash","This code will not compile"],
-            rightAnswer: 2
-        ))
+    func question() -> [QuestionModel]{
+
+        let questions = [
+            QuestionModel(questionTitle: "What output will be produced by the code below?",
+                          questionImage: "first.jpg",
+                          answers: ["This is a subset","This is not a subset","Nothing will be output","This code will compile but crash","This code will not compile"],
+                          rightAnswer: 2),
+            QuestionModel(questionTitle: "1",
+                          questionImage: "first.jpg",
+                          answers: ["This is a subset","This is not a subset","Nothing will be output","This code will compile but crash","This code will not compile"],
+                          rightAnswer: 2),
+            QuestionModel(questionTitle: "2",
+                          questionImage: "first.jpg",
+                          answers: ["This is a subset","This is not a subset","Nothing will be output","This code will compile but crash","This code will not compile"],
+                          rightAnswer: 2),
+            QuestionModel(questionTitle: "3",
+                          questionImage: "first.jpg",
+                          answers: ["This is a subset","This is not a subset","Nothing will be output","This code will compile but crash","This code will not compile"],
+                          rightAnswer: 2)
+        ]
 
         return questions
     }
+
+
 }
--- InterviewQuiz/ViewController.swift
+++ InterviewQuiz/ViewController.swift
@@ -11,19 +11,96 @@ import UIKit
 class ViewController: UIViewController {
 
     //Outlets
+    @IBOutlet weak var scoreLabel: UIBarButtonItem!
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var qestionTitleLabel: UILabel!
     @IBOutlet weak var questionImage: UIImageView!
 
+    @IBOutlet weak var optionA: RoundedButton!
+    @IBOutlet weak var optionB: RoundedButton!
+    @IBOutlet weak var optionC: RoundedButton!
+    @IBOutlet weak var optionD: RoundedButton!
+    @IBOutlet weak var optionE: RoundedButton!
+
+    @IBOutlet var optionButtons: [RoundedButton]!
+
+
+
     //Variables
-    var questions = DataService.instance.getQuestion()
+    let dataService = DataService()
+    var questions = [QuestionModel]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        shuffleQuestions()
+        pickQuestion()
+        print(questions.count)
+    }
 
-        for q in questions {
-            questionImage.image = UIImage(named: q.questionImage) 
+    private func shuffleQuestions() {
+        let questions = dataService.question()
+        for _ in 0..<questions.count + 1 {
+            let randomQuestion = questions.randomElement()
+
+            self.questions.append(randomQuestion!)
         }
     }
+
+    var questionNumber = Int()
+    func pickQuestion() {
+        if questions.count > 0 {
+            questionNumber = 0
+            qestionTitleLabel.text = questions[questionNumber].questionTitle
+            questionImage.image = UIImage(named: questions[questionNumber].questionImage)
+
+            for i in 0..<optionButtons.count {
+                optionButtons[i].setTitle(questions[questionNumber].answers[i], for: .normal)
+            }
+        } else {
+
+            print("finish!!!!!!!")
+        }
+    }
+
+
+
+    @IBAction func submitAnswerBtnWasTapped(_ sender: UIButton) {
+        checkRightAnswer()
+        questions.remove(at: questionNumber)
+        pickQuestion()
+    }
+
+    func checkRightAnswer() {
+        let rightAnswer = questions[questionNumber].rightAnswer
+        if rightAnswer == selectedAnswer {
+            print("ok")
+        } else {
+            print("no")
+        }
+    }
+
+    var selectedAnswer: Int = 0
+    @IBAction func optionButtonWasTapped(_ sender: UIButton) {
+        let isAlreadySelected = sender.isSelected
+        resetButtonStates()
+        if !isAlreadySelected {
+            sender.isSelected = true
+            sender.backgroundColor = #colorLiteral(red: 0.9960784314, green: 0.6980392157, blue: 0.03921568627, alpha: 1)
+            selectedAnswer = sender.tag
+        }
+    }
+    
+
+    func resetButtonStates() {
+        for button in optionButtons {
+            button.isSelected = false
+            button.backgroundColor = #colorLiteral(red: 0.5808190107, green: 0.0884276256, blue: 0.3186392188, alpha: 1)
+        }
+    }
+
+
+
 }
 
 
